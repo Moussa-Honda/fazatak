288eef8c20afc84c88e8378d02a7af749b15fca3
Moussa-Honda <honda.druid@gmail.com>
2026-09-11 19:16:34 -0500

    Update backupService.js

--- src/services/backupService.js
+++ src/services/backupService.js
@@ -319,7 +319,7 @@ export const shareBackup = async () => {
         try {
           await navigator.share({
             title: BACKUP_FILE_NAME,
-            text: 'نسخة احتياطية من بيانات اقساطي',
+            text: 'نسخة احتياطية من بيانات أقساطي',
             files: [file]
           });
           return {
@@ -355,7 +355,7 @@ export const shareBackup = async () => {
 
     await Share.share({
       title: BACKUP_FILE_NAME,
-      text: 'نسخة احتياطية من بيانات اقساطي',
+      text: 'نسخة احتياطية من بيانات أقساطي',
       url: uri,
       files: [uri],
       dialogTitle: 'مشاركة النسخة الاحتياطية'
